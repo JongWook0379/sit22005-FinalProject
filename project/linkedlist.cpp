5740dc34f668ca9f1205f15c2d2f992550ce3fc7
JongWook Park <21500292@handong.edu>
2017-12-15 05:11:24 +0000

    project

--- project/linkedlist.cpp
+++ project/linkedlist.cpp
@@ -37,5 +37,54 @@ StringNode::StringNode()
     
 }
 
+
+StringNode::STRING_NODE(char* val, Node* next)
+:Node(next, STRING_NODE)
+{
+    value = new char[strlen(val)+1];
+    strcpy(vale, val);
+    value[strlen(val)] = '\0'
+}
+
+StringNode::~StringNode()
+{
+    delete [] value;
+}
+
+char* StringNode::get_value()
+{
+    return value;
+}
+
+void StringNode::set_value(char* val)
+{
+    if(value != val)
+    {
+        delete [] value;
+        value = new char[strlen(val)+1];
+        strcpy(value, val);
+        value[strlen(val)] = '\0'
+    }
+}
+
+
 //////////////////////////////////////////////////////////////
 // LinkedList Manager
+linkedlist::linkedlist()
+:pHead(nullptr)
+{
+    
+}
+
+void linkedlist::InsertToFront(int val)
+{
+    IntegerNode* inode = new IntegerNode(val, pHead);
+    pHead = inode;
+}
+
+
+
+
+
+
+
