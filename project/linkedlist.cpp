639cbdf6b4434314687c3e56a67c8db23a1c0c0d
JongWook Park <21500292@handong.edu>
2017-12-15 05:01:47 +0000

    project

--- project/linkedlist.cpp
+++ project/linkedlist.cpp
@@ -4,14 +4,38 @@
 
 //////////////////////////////////////////////////////////////
 // Node
+Node::Node(Node* next, NodeType node)
+:pNext(next), node_type(node)
+{
+    
+}
 
 
 //////////////////////////////////////////////////////////////
 // IntegerNode
+IntegerNode::IntegerNode()
+:Node(nullptr, INT_NODE)
+{
+    
+}
+
+int IntegerNode::get_value()
+{
+    return value;
+}
+
+void IntegerNode::set_value(int val)
+{
+    value=val;
+}
 
 //////////////////////////////////////////////////////////////
 // StringNode
-
+StringNode::StringNode()
+:Node(nullptr, STRING_NODE)
+{
+    
+}
 
 //////////////////////////////////////////////////////////////
 // LinkedList Manager
